9ad169d46da78923c4e823f0f1ed00b5acf9396e
epicarts <0505zxc@gmail.com>
2019-06-07 17:38:07 +0900

    add code explain

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,6 +5,7 @@ import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        #chat/routing.py 에 정의된 URL 파라미터에서 roomName을 얻음
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
@@ -23,11 +24,13 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name
         )
 
+    # 웹소켓으로부터 메세지를 받아 처리하는 부분이다.
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        # 아래에서는 그룹으로 메세지를 보내고 있다.
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -37,6 +40,7 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
+    # 위의 receive 메서드에서 그룹으로 메세지를 보내면 그 메세지를 받아 처리하는 부분이다.
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
