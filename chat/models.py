621f7214fb8220091cb7f68ebc0c64ffb00f6e3a
epicarts <0505zxc@gmail.com>
2019-06-16 17:03:10 +0900

    upload test

---
+++ chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+class Roon(models.Model):
+    name = models.TextField()
+    label = models.SlugField(unique=True)
+
+    def __unicode__(self):
+        return self.label
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages')
+    handle = models.TextField()
+    message
