1d7959dec6b31ae1405f317f36e2d64f6422cddf
seohui <skfhddl003@gmail.com>
2019-07-18 17:09:31 +0900

    homelist

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Posting
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def main(request):
     print(request)
     return render(request, 'main/main.html', {'title': 'main page', 'body': 'this is main page'})
+@login_required
+def seo(request):
+    postings=Posting.objects.all()
+    context={'postings':postings}
+    return render(request,'main/seo.html',context)
