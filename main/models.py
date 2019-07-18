cb817b1a237a02b9f4ff425b2370ebd658c52d6f
YoungHo Choi <0505zxc@gmail.com>
2019-07-18 17:11:39 +0900

    Revert "homelist"

--- main/models.py
+++ main/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Posting(models.Model):
-    group=models.CharField(max_length=30)
-    roomname=models.CharField(max_length=100)
-    name=models.CharField(max_length=10)
-    nowcount=models.IntegerField(default=1)
-
-    def __str__(self):
-        return self.name
--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,4 @@ app_name='main'
 urlpatterns = [
     #로그인 성공 시 => profile로 리다이텍트 됨 settings
     path('', views.main),
-    path('seo/',views.seo)
 ]
