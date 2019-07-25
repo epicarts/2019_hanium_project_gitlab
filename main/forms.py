02b59337c6c154b27cfab6f179234cd6f6bdba09
seohui <skfhddl003@gmail.com>
2019-07-25 22:51:50 +0900

    final

--- main/forms.py
+++ main/forms.py
@@ -4,5 +4,4 @@ from .models import Posting
 class CreateMain(forms.ModelForm):
     class Meta:
         model=Posting
-
         fields=['group','roomname','name','nowcount','start_date','end_date']
--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from .forms import CreateMain
 def main(request):
     print(request)
     return render(request, 'main/main.html', {'title': 'main page', 'body': 'this is main page'})
+
 @login_required
 def seo(request):
     postings=Posting.objects.all()
@@ -14,14 +15,14 @@ def seo(request):
     return render(request,'main/seo.html',context)
 
 def createMain(request):
-    if request.method == 'post':
+    if request.method == 'POST':
         form = CreateMain(request.POST)
-
-        if form.is_valid():
+        if form.is_valid():#폼 형식이 맞으면
             form.save()
-            return redirect('main')
+            return redirect('main:seo')#저장하고 main으로 보냄
         else:
-            return redirect('seo')
-    else:
+            #맞지않으면 페이지에 계속 유지 시켜놓기
+            return redirect('main:createMain')
+    else:#post 요청이 아닐경우 form을 상속받아서, createMain.html로 전달
         form = CreateMain()
-        return render(request,'main/seo.html',{"form":form})
+        return render(request,'main/createMain.html',{"form":form})
