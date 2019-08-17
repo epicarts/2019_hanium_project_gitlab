9529ed7e8196a68660447bb98e18fe230e979fa2
epicarts <0505zxc@gmail.com>
2019-08-18 04:08:35 +0900

    add permission

--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,8 @@ import json
 from .models import Room
 from django.utils import timezone
 from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth.models import Group
 
 # def index(request):
 #     return render(request, 'chat/index.html', {})
@@ -22,39 +24,47 @@ def my_view(request):
 
 has_perm() 함수와 동일하게, . 과 같이 지정하면 된다. `permission_required()` 함수 또한 `login_url` 를 인자로 받을 수 있으며, `raise_exception`이 주어지면, `PermissionDenied` 에러가 발생하며 로그인 페이지로 이동하는 대신 403에러 페이지를 보여주게 된다.
 '''
-# from django.contrib.auth.decorators import user_passes_test
-
-# def email_check(user):
-#     return user.email.endswith('@example.com')
-
-# #룸에 대한 pk 값을 체크. + 해당 pk에 대해 권한이 있는지 체크. 
-# #만약 권한이 없다면, 페이지를 보여 줄 수 없다고 함. 
-# #pk 체크하기전에 passwd 체크. 
-# #추가로 관리자 퍼미션 체크 또는 이건 템플릿에 띄우기
-# @user_passes_test(email_check)
-# def my_view(request, pk):
-
-
 
 @login_required
 def room(request, room_pk):
-    # 1. requesst.user has room permission?
-    # 2. 
+    '''
+    GET
+    1. 현재 유저가 펴미션이 있나 체크
+    2. 권한 있음: 룸 입장
+    3. 권한 없음: redirect 후 패스워드 입력
 
-    #현재 로그인 되어있는 계정을 기준으로 퍼미션 체크를 한다.
-    perimssion_codename = 'main.room_'+ str(room_pk) +'_view'
-    
-    #퍼미션을 가지고 있다면 정상적으로 페이지를 보여줌
-    if request.user.has_perm(perimssion_codename):
-        room = Room.objects.get(pk=room_pk)
-        room_messages = reversed(room.messages.order_by('-timestamp')[:5])
-        return render(request, 'chat/room.html', {
-            'room_pk_json': mark_safe(json.dumps(room_pk)),
-            'messages':room_messages,
-            'room':room,#데이터 베이스를 통째로 넘겨줌
-        })
-    #퍼미션을 가지고 있지 않다면,
-    else:
-        messages.add_message(request, messages.ERROR, '권한이 없습니다.')
-        return redirect('main:RoomList')
+    POST
+    1. 패스워드 요청이 들어오면 체크(POST)
+    2. 패스워드가 맞다:  그룹에 속하게 함(그룹 has 권한)
+    3. 패스워드가 다르다: redirect 후 패스워드 틀림페이지
+    '''
+    if request.method == 'GET':
+        #현재 로그인 되어있는 계정을 기준으로 퍼미션 체크를 한다.
+        perimssion_codename = 'main.room_'+ str(room_pk) +'_view'
 
+        #퍼미션을 가지고 있다면 정상적으로 페이지를 보여줌
+        if request.user.has_perm(perimssion_codename):
+            room = Room.objects.get(pk=room_pk)
+            room_messages = reversed(room.messages.order_by('-timestamp')[:5])
+            return render(request, 'chat/room.html', {
+                'room_pk_json': mark_safe(json.dumps(room_pk)),
+                'messages':room_messages,
+                'room':room,
+            }) 
+        else:#처음 방 입장을 하면 패스워드가 없으므로 패스워드 입력 페이지를 랜더링
+            messages.add_message(request, messages.INFO, room_pk)#request에 message 추가
+            return redirect('main:RoomList')
+
+    elif request.method == 'POST': #패스워드 인증 요청이 패스워드 입력 페이지로부터 AJAX 요청이 들어오면 
+        print('post request BY AJAX')
+        if request.POST['password'] == Room.objects.get(pk=room_pk).password:# 파라미터로 입력받은 값을 방의 패스워드와 비교함 ,
+            #접속한 유저를 패스워드를 통과한 방 그룹에 속하게 함.
+            groupname = 'room_'+ str(room_pk) +'_group'
+            new_group = Group.objects.get(name=groupname) 
+            request.user.groups.add(new_group)
+            # 200 == success, 리다이렉션 처리는 js에서 함
+            print("접속 성공")
+            return HttpResponse('인증 되었습니다.',content_type='application/json', status=200) # http status 200
+            
+        else:#패스워드가 다르면 리다이렉트...
+            return HttpResponse('패스워드가 잘못 되었습니다.', content_type='application/json', status=400) #  http status 400 잘못된 패스워드, 리다이렉션 처리는 js에서 함
--- main/views.py
+++ main/views.py
@@ -3,26 +3,24 @@ from django.views import View
 from .models import Room
 from django.contrib.auth.decorators import login_required
 from .forms import CreateMain
+
 from django.contrib.auth.models import User, Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
 from django.core.signing import Signer
 
+from django import template
+
+import datetime
+from django import template
+
+from django.contrib import messages
+
 # Create your views here.
 @login_required
 def main(request):
-    RoomList=Room.objects.all()
-    return render(request, 'main/main.html', {'RoomList':RoomList, 'title': 'main page', 'body': 'this is main page'})
-
-
-
-# def deleteRoom(request, room_id):
-#     if room.owner == requset.suer:
-#         room.delete () 
-#         return 
-#     return "권한이 없ㅅ브니다" 
-
-    
+    RoomList=reversed(Room.objects.all()) #가장 최근에 만들어진 방을 위로 정렬..
+    return render(request, 'main/main.html', {'RoomList':RoomList})
 
 @login_required
 def createMain(request):
@@ -34,15 +32,12 @@ def createMain(request):
                 post = form.save(commit=False) # 함수 호출 지연
                 post.author = request.user # 유저 id 필드는 유저로 부터 입력 받지 않고 프로그램으로 채워 넣는다
                 post.save()
-
-                print(post.pk)
                 '''
                 그룹 및 권한 추가
                 그룹: room_{pk}_group
                 권한: room_{pk}_view  / room_{pk}_delete
                 '''
                 groupname = 'room_'+ str(post.pk) +'_group'
-
                 #그룹이 없다면 생성하거나 가져옴. 그룹 이름은 방을 생성할 때 방 그룹은 id room_id 
                 new_group, created = Group.objects.get_or_create(name=groupname)
                 #그룹에 권한(permission 생성 및 추가)
@@ -52,7 +47,6 @@ def createMain(request):
                 permission = Permission.objects.create(codename=perimssion_codename, name=perimssion_name, content_type=content_type)
                 new_group.permissions.add(permission)#그룹에 권한 추가
                 request.user.groups.add(new_group)#그룹에 개인 추가
-
 
                 # Code to add permission to group 
                 # room 모델을 삭제할 수 있는 permission을 만듬
@@ -64,15 +58,14 @@ def createMain(request):
 
                 #redirect('/detail/'+str(cat.pk)) 저장하고 만들어진 페이지로
                 return redirect('main:RoomList')#저장하고 main으로 
-
             else:##password와 password_check가 다를 것을 대비하여 error를 지정해준다.
+                messages.add_message(request, messages.INFO, "패스워드가 알치하지 않습니다.")
                 return redirect('main:createMain')
 
-            
-            
-
-        else:#맞지않으면 페이지에 계속 유지 시켜놓기
+        else:#폼 형식이 맞지 않으면 맞지 않으면 메시지와 redirect
+            messages.add_message(request, messages.INFO, "폼 형식이 맞지 않습니다. 다시 입력해 주세요.")
             return redirect('main:createMain')
-    else:#post 요청이 아닐경우 form을 상속받아서, createMain.html로 전달
+
+    if request.method == 'GET': # GET일 경우 form을 상속받아서, createMain.html로 전달
         form = CreateMain()
         return render(request,'main/createMain.html',{"form":form})
